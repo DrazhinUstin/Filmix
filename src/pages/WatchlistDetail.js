430ee98dcbefa4382c0690516f306c5b60f6caf6
DrazhinUstin <zmagar91@gmail.com>
2023-04-19 06:03:30 +0300

    Update watchlist detail page

--- src/pages/WatchlistDetail.js
+++ src/pages/WatchlistDetail.js
@@ -3,27 +3,34 @@ import { collection, getCountFromServer, getDocs, writeBatch } from 'firebase/fi
 import { auth, db } from '../firebase';
 import { Title, GridList, RedButton, Modal } from '../components';
 import { toast } from 'react-toastify';
+import { breakpoints } from '../GlobalStyles';
 import styled from 'styled-components';
 
 const WatchlistDetail = () => {
     const [isLoading, setIsLoading] = useState(true);
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState({ watchlist: 0, ratings: 0 });
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
         setIsLoading(true);
-        getCountFromServer(collection(db, `users/${auth.currentUser.uid}/watchlist`))
-            .then((snapshot) => setCount(snapshot.data().count))
+        Promise.allSettled([
+            getCountFromServer(collection(db, `users/${auth.currentUser.uid}/watchlist`)),
+            getCountFromServer(collection(db, `users/${auth.currentUser.uid}/ratings`)),
+        ])
+            .then(([val1, val2]) => {
+                setCount({
+                    watchlist: val1?.value?.data()?.count || 0,
+                    ratings: val2?.value?.data()?.count || 0,
+                });
+            })
             .catch((error) => toast.error(error.message))
             .finally(() => setIsLoading(false));
     }, []);
 
-    const clearWatchlist = async () => {
+    const clearCollection = async (name) => {
         setIsLoading(true);
         try {
-            const { docs } = await getDocs(
-                collection(db, `users/${auth.currentUser.uid}/watchlist`)
-            );
+            const { docs } = await getDocs(collection(db, `users/${auth.currentUser.uid}/${name}`));
             const chunks = [];
             for (let i = 0; i < docs.length; i += 500) {
                 chunks.push(docs.slice(i, i + 500));
@@ -33,7 +40,7 @@ const WatchlistDetail = () => {
                 chunk.forEach((doc) => batch.delete(doc.ref));
                 await batch.commit();
             });
-            setCount(0);
+            setCount({ ...count, [name]: 0 });
         } catch (error) {
             toast.error(error.message);
         }
@@ -42,24 +49,26 @@ const WatchlistDetail = () => {
 
     return (
         <Wrapper>
-            <Title margin='0 0 2rem'>watchlist:</Title>
+            <Title margin='0 0 2rem'>statistic:</Title>
             <GridList fontWeight='normal'>
-                <li>
-                    Total items: <span>{count}</span>
-                </li>
+                {Object.keys(count).map((key) => (
+                    <li key={key}>
+                        {key[0].toUpperCase() + key.slice(1)}: <span>{count[key]}</span>
+                        <RedButton
+                            onClick={() => setIsModalOpen({ key })}
+                            disabled={isLoading || !count[key]}
+                            $withBorder
+                        >
+                            clear
+                        </RedButton>
+                    </li>
+                ))}
             </GridList>
-            <RedButton
-                onClick={() => setIsModalOpen(true)}
-                disabled={isLoading || !count}
-                $withBorder
-            >
-                clear watchlist
-            </RedButton>
             {isModalOpen && (
                 <Modal
-                    title='Are you sure that you want to clear your watchlist?'
+                    title={`Are you sure that you want to clear all your ${isModalOpen.key}?`}
                     closeModal={() => setIsModalOpen(false)}
-                    callback={clearWatchlist}
+                    callback={() => clearCollection(isModalOpen.key)}
                 />
             )}
         </Wrapper>
@@ -72,12 +81,16 @@ const Wrapper = styled.article`
     text-align: center;
     ${GridList} {
         justify-content: center;
-        margin: 2rem 0;
         text-align: left;
         li {
-            grid-template-columns: 8rem auto;
+            grid-template-columns: 1fr 8rem 8rem;
+            align-items: center;
             span {
                 color: var(--clr-green);
+                text-align: center;
+            }
+            @media ${breakpoints.xsm} {
+                grid-template-columns: 1fr 4rem 6rem;
             }
         }
     }
